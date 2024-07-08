f0809269abd17717744d7bbe2796bd54d306f0f1
Stanley Wijaya <110814509+stanleywijaya12@users.noreply.github.com>
2024-07-08 16:37:07 +0700

    Add full function on Etch a Sketch

--- script.js
+++ script.js
@@ -2,18 +2,47 @@ let get_container = document.getElementsByClassName("container");
 let container = get_container[0];
 
 function addDiv() {
-  number_of_squares = 16;
+  if (container.children.length > 0) {
+    container.innerHTML = "";
+  }
+  let number = prompt("Choose the number of grids");
 
-  for (let i = 0; i < number_of_squares; i++) {
-    let createRow = document.createElement("div");
-    createRow.className = "row";
-    container.appendChild(createRow);
-    for (let j = 0; j < number_of_squares; j++) {
-      let createDiv = document.createElement("div");
-      createDiv.className = "grid";
-      createRow.appendChild(createDiv);
+  if (number > 100) {
+    alert("IS TOO MUCH BRO!");
+  } else {
+    number_of_squares = number;
+
+    for (let i = 0; i < number_of_squares; i++) {
+      let createRow = document.createElement("div");
+      createRow.className = "row";
+      container.appendChild(createRow);
+      for (let j = 0; j < number_of_squares; j++) {
+        let createDiv = document.createElement("div");
+        createDiv.className = "grid";
+        createRow.appendChild(createDiv);
+      }
     }
+    paint();
   }
 }
 
-addDiv();
+function randomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function paint() {
+  let get_grid = document.getElementsByClassName("grid");
+  Array.from(get_grid).forEach((element) => {
+    element.addEventListener("mouseover", function () {
+      // get_grid[element].classList.add("colorify");
+      let color = randomColor();
+      elem = element;
+      elem.style.backgroundColor = color;
+    });
+  });
+}
